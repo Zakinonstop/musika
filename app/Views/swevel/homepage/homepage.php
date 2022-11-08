30c9049775de72ad81d7481dfded788d4c90c66e
Arga Bimantara <argabimantara8@gmail.com>
2022-11-08 08:47:36 +0700

    arga

--- app/Views/swevel/homepage/homepage.php
+++ app/Views/swevel/homepage/homepage.php
@@ -172,7 +172,6 @@ function circle1()
     <div id="card-list-course" class="row mt-5 pt-5"></div>
     <input type="hidden" name="" id="limit-course" value="3">
     <div class="d-flex justify-content-center mt-3">
-        <a href="/course" class="btn btn-purple px-5">Lihat semua</a>
         <a href="/course" class="btn btn-purple px-5 btn-more-course hide">Lihat semua</a>
     </div>
 </div>
